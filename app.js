d1f32ef25212246986fa04f689fb18e70cd3a027
Ahmed Alkooheji <ahmed@kooheji.info>
2024-02-19 16:38:20 +0300

    Uptime added.

--- app.js
+++ app.js
@@ -1,10 +1,31 @@
 const express = require("express");
 const app = express();
+const PORT = 3000;
+
+//Uptime Calculator
+app.get("/uptime/:unixTime", (req, res) => {
+  const unixTime = parseInt(req.params.unixTime);
+  const currentTime = Math.floor(Date.now() / 1000);
+  const timeDiff = currentTime - unixTime;
+
+  const days = Math.floor(timeDiff / (60 * 60 * 24));
+  const hours = Math.floor((timeDiff % (60 * 60 * 24)) / (60 * 60));
+  const minutes = Math.floor((timeDiff % (60 * 60)) / 60);
+  const seconds = timeDiff % 60;
+
+  const formattedTime = `${days.toString().padStart(3, "0")}D ${hours
+    .toString()
+    .padStart(2, "0")}H ${minutes.toString().padStart(2, "0")}M ${seconds
+    .toString()
+    .padStart(2, "0")}S`;
+
+  res.send(formattedTime);
+});
 
 app.get("/", (req, res) => {
   res.send("Hello, World!");
 });
 
-app.listen(3000, () => {
-  console.log("Server running on http://localhost:3000");
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
